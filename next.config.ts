7b9c748dd7f4063141f097a4489977bec8ad7483
Tobias Lohse <MrLoh@users.noreply.github.com>
2025-01-04 10:37:35 -0600

    setup

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
-import type { NextConfig } from "next";
+import createMDX from '@next/mdx';
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
 };
 
-export default nextConfig;
+const withMDX = createMDX({
+  options: {
+    // @ts-expect-error -- bug in the @next/mdx package types
+    remarkPlugins: [['remark-gfm'], ['remark-frontmatter'], ['remark-mdx-frontmatter']],
+    rehypePlugins: [],
+  },
+});
+
+export default withMDX(nextConfig);
